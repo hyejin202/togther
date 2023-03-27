1c1802691700345ac6cdf27a9da4897cbf6d1219
hyejin202 <123609520+hyejin202@users.noreply.github.com>
2023-03-27 14:00:22 +0900

    1

---
+++ src/togther/aa.java
@@ -0,0 +1,12 @@
+package togther;
+
+import java.util.*;
+
+public class aa {
+	public static void main(String[] args) {
+		for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
+			type type = (type) iterator.next();
+			
+		}
+	}
+}
